5f6acbaa74cc4c70e170b53a98eb8f4a96b09d2e
Swasti <swastijain16@outlook.com>
2021-09-19 17:43:47 +0530

    day8

---
+++ src/day8.java
@@ -0,0 +1,115 @@
+import java.util.*;
+public class day8 {
+    public static void main(String[] args) {
+        //int r=1;
+        Scanner s = new Scanner(System.in);
+
+//
+//        System.out.println("enter n:");
+//        int n = s.nextInt();
+//        while(r<=n){
+//            int c = 1;
+//            while(c<=r){
+//                System.out.print("* ");
+//                c = c +1;
+//            }
+//            System.out.println();
+//            r = r +1;
+//        }
+
+//----------------------------------------------------
+//        System.out.println("enter n:");
+//        int n = s.nextInt();
+//        while(r<=n){
+//            int c = 1;
+//            while(c<=n){
+//                System.out.print("* ");
+//                c = c +1;
+//            }
+//            System.out.println();
+//            r = r +1;
+//        }
+//---------------------------------------------------
+//
+//        System.out.println("enter n:");
+//
+//        int n = s.nextInt();
+//        int r = 1;
+//
+//        while(r<=n){
+//            int c =n;
+//            while(c>=r){
+//                System.out.print("* ");
+//                c = c-1;
+//            }
+//            System.out.println();
+//            r= r+1;
+//        }
+//-------------------------------------------------
+//        System.out.println("enter n:");
+//        int n = s.nextInt();
+//        int r = 1;
+//        int m=n;
+//        int rr=r;
+//        //int cc=c;
+//        while(r<=n){
+//            int c = 1;
+//            while(c<=r){
+//                System.out.print("* ");
+//                c = c +1;
+//            }
+//            System.out.println();
+//            r = r +1;
+//        }
+//
+//
+//        while(rr<=m){
+//            int cc =m-1;
+//            while(cc>=rr){
+//                System.out.print("* ");
+//                cc = cc-1;
+//            }
+//            System.out.println();
+//            rr= rr+1;
+//        }
+//-----------------------------------------------
+//        System.out.println("enter n:");
+//        int n = s.nextInt();
+//        int r = 1;
+//        int i = 0;
+//        while(r<=n){
+//            int c= 1;
+//            while(c<=r){
+//                i++;
+//                System.out.print(" "+i);
+//                c = c+1;
+//
+//            }
+//            System.out.println();
+//            r = r+1;
+//
+//        }
+//---------------------------------------------
+        System.out.println("enter n:");
+        int n = s.nextInt();
+        int r = 1;
+        int i = 1;
+        while(r<=n){
+            int c= 1;
+            while(c<=r){
+
+                System.out.print(" "+c);
+                c = c+1;
+               // i++;
+            }
+            System.out.println();
+            r = r+1;
+//--------------------------------------------------
+
+
+        }
+
+
+
+    }
+}
